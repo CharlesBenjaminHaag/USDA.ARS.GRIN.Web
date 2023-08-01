9c79066d418d62c376fd9612bed76e471d1c7083
Benjamin Haag <Benjamin.Haag@usda.gov>
2023-08-01 16:19:20 -0400

    Trac #2847

--- USDA.ARS.GRIN.Web.UI.v2/Controllers/SiteController.cs
+++ USDA.ARS.GRIN.Web.UI.v2/Controllers/SiteController.cs
@@ -18,22 +18,22 @@ namespace USDA.ARS.GRIN.Web.UI.v2.Controllers
             return View();
         }
 
-        public PartialViewResult _List(string formatCode="")
-        {
-            try
-            {
-                SiteViewModel viewModel = new SiteViewModel();
-                viewModel.SearchEntity.FormatCode = formatCode;
-                viewModel.SearchEntity.IDList = "1,2,3,4,5,6,7,8,9,13,15,16,17,19,22,24,26,29,31,34,37,40";
-                viewModel.Search();
-                return PartialView(viewModel);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex);
-                return PartialView("~/Views/Error/_InternalServerError.cshtml");
-            }
-        }
+        //public PartialViewResult _List(string formatCode="")
+        //{
+        //    try
+        //    {
+        //        SiteViewModel viewModel = new SiteViewModel();
+        //        viewModel.SearchEntity.FormatCode = formatCode;
+        //        viewModel.SearchEntity.IDList = "1,2,3,4,5,6,7,8,9,13,15,16,17,19,22,24,26,29,31,34,37,40";
+        //        viewModel.Search();
+        //        return PartialView(viewModel);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Log.Error(ex);
+        //        return PartialView("~/Views/Error/_InternalServerError.cshtml");
+        //    }
+        //}
         [HttpPost]
         public PartialViewResult _Detail(int siteId)
         {
